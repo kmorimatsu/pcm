33001953e65ae8758c0188a3af237678e650b7e7
Katsumi <kmorimatsu@users.sourceforge.jp>
2022-05-07 22:41:23 -0700

    Sawtooth wave 440 Hz

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <math.h>
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
 
 int main() {
 	unsigned int port,slice,channel;
+	int i;
 	stdio_init_all();
 	sleep_ms(3000);
 	printf("Hello World!");
@@ -18,12 +20,23 @@ int main() {
 	// f = sysclock / (( wrap + 1 ) x clkdiv)
 	// clkdiv = sysclock / ( wrap + 1 ) x f 
 	// ( 1.0 <= clkdiv < 256.0 ) 
-	pwm_set_clkdiv(slice,62500.00/440.0);
+	pwm_set_clkdiv(slice,1.0);
 	// 1000 cycles PWM
-	pwm_set_wrap(slice, 1999);
+	pwm_set_wrap(slice, 255);
 	// Set duty
-	pwm_set_chan_level(slice, channel, 1000);
+	pwm_set_chan_level(slice, channel, 128);
 	// Enable
 	pwm_set_enabled(slice, true);
-
+	
+	while(1){
+		for(i=0;i<15;i++){
+			pwm_set_chan_level(6, PWM_CHAN_A,i*16);
+			sleep_us(76);
+		}
+		for(i=15;0<i;i--){
+			pwm_set_chan_level(6, PWM_CHAN_A,i*16);
+			sleep_us(76);
+		}
+		
+	}
 }
